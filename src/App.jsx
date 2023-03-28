04578b8b4646148f1c31e86fc5c3d12e0295ac4a
Purvesh Rathod <purvesh.r@dal.ca>
2023-03-28 02:27:38 -0300

    The axios import and useEffect hook were not being used in the App component, so they were removed to improve code readability and maintainability.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/public/vite.svg'
 import './App.css'
-import axios from "axios";
-import {useEffect} from "react";
 
 function App() {
-    useEffect(() => {
-        axios.get('https://c356-134-41-179-198.ngrok.io/events?query=system&type=Free').then((response) => {
-            console.log(response.data);
-        })
-    }, [])
     return (
         <div className="App">
             <div>
