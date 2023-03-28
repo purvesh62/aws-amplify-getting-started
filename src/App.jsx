c50ec1a5adf06aa1d51512dec7044b49875c6d98
Purvesh Rathod <purvesh.r@dal.ca>
2023-03-28 01:56:02 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/public/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>AWS Amplify Getting Started</h1>
+    </div>
+  )
+}
+
+export default App
